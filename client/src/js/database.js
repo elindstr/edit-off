7c777b435ba3e0714d0eb9cdd0ba38191e00a5ea
elindstr <elindstr@gmail.com>
2024-04-25 23:31:55 -0700

    fixed db put and get bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,18 +12,18 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Adds logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: 1, jate: content }); // rewrite id:1 only
   const result = await request;
   console.log('Saved:', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// Adds logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   const contactDb = await openDB('jate', 1);
@@ -31,8 +31,10 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
+
+  await tx.done;
   console.log('Results:', result);
-  return result;
+  return result.length > 0 ? result[0].jate : null; // prevent error on load
 }
 
 initdb();
